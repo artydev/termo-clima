79585189839e9ae59938daf20622b8fbd48652eb
DI DIO Salvatore (SICCRF) <Salvatore.DI-DIO@dgccrf.finances.gouv.fr>
2024-07-27 22:44:58 +0200

    Adjust Navbar

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,8 +3,8 @@ import { m  } from "umai"
 import '@master/css';
 
 const Hero = () => 
-    <div class="mt:0rem">
-        <img src="hero.jpg" class="position:relative"/>
+    <div class="mt:4rem">
+        <img src="hero.jpg" class="position:relative z-index:0"/>
     </div>
 
 export  { Hero }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { m  } from "umai"
 import '@master/css';
 
 const Navbar = () => 
-    <div className="d:block box-shadow:1|1|10|rgb(162,162,162)  h:4rem line-height:4rem bg:white; position:fixed;w:100vw position:fixed w:100vw">
+    <div className="d:block box-shadow:1|1|10|rgb(162,162,162)  h:4rem line-height:4rem bg:white w:100vw position:fixed w:100vw  z-index:2">
         <h1>Navbar!!</h1>
     </div>
 
