52b271fe070725e342e27f2f0f54e36b0cafe766
DI DIO Salvatore (SICCRF) <Salvatore.DI-DIO@dgccrf.finances.gouv.fr>
2024-07-27 22:32:35 +0200

    adjust main css

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ import '@master/css';
 
 const Hero = () => 
     <div class="mt:0rem">
-        <img src="hero.jpg" />
+        <img src="hero.jpg" class="position:relative"/>
     </div>
 
 export  { Hero }
